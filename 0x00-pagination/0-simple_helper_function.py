0db6a4f310b20692c7eacbe8bc667abe2816a637
Mary-Maina <richellwanjiru@gmail.com>
2024-11-05 14:11:38 +0000

    0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""
+Given a list of items we want to break them down
+into pages, and number of items in that particular list
+"""
+def index_range(page: int, page_size: int) -> tuple:
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
